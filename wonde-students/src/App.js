562ffc7349dc4e0160520ca9f2e60ccbe7b483d9
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-03-31 15:26:18 +0100

    Created React app
    I decided to do this task using React and Axios
    I created React app and added a component called students
    I made sure I add the component to the App.js one
    I added .env file to keep my token and the school id
    After that I worked on the fetch requests to the API in order to be able to get some data
    I added an axios request with bearer authorization header as that was suggested as best practice in the documentation
    I added 3 calls to the API:
     - first one to get the students
     - second one to get the teachers
     - third one to get the classes
    It all worked and I rendered those to the screen.
    However, I realised that I would not get the students in a particular class that way and I have to think of another method.
    I found out that I have to use some of the included properties to do that. I spent a lot of time to work it out as I was not sure how to use includes and could not find much information in the documentation. At the end I managed to get to the data I need by adding include= and whatever data I need from the meta data.
    So I believe I will have to get rid of all of the data I have on the FE apart from the teachers names and add a function which will get called on click to request the details of this particular teacher

---
+++ wonde-students/src/App.js
@@ -0,0 +1,17 @@
+import Students from './Students'
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <Students />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ wonde-students/src/Students.js
@@ -0,0 +1,193 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import env from 'react-dotenv'
+import { useState } from 'react'
+
+const token = process.env.REACT_APP_TOKEN
+const school_id = process.env.REACT_APP_SCHOOL_ID
+
+const teacher_ids = [
+    'A1375078684',
+    'A1624134916',
+    'A1796016840'
+]
+const teacher_id = "A1976397528"
+const class_ids = [
+    'A575444512',
+    'A1253639300',
+    'A344762973'
+]
+
+const class_id = 'A575444512'
+
+export default class Students extends Component {
+    state = {
+        students: [],
+        employees: [],
+        employeesClasses: [],
+        classesE: [],
+    }
+
+    componentDidMount() {
+        // const url = 'https://jsonplaceholder.typicode.com/users'
+        const url = `https://api.wonde.com/v1.0/schools/${school_id}/students?include=classes`
+        const url2 = `https://api.wonde.com/v1.0/schools/${school_id}/employees?include=classes`
+        const url3 = `https://api.wonde.com/v1.0/schools/${school_id}/classes?include=students`
+
+
+//STUDENTS
+        axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${token}` 
+              }
+        })
+          .then(res => {
+            // let students = res.data;
+            this.setState({ students: res.data.data });
+            console.log(this.state.students)
+            console.log(res.data.data[0].classes.data[0].name)
+            // console.log(students.data)
+            // students = students.data
+            // console.log(students.data[1].initials)
+            // console.log(students.data[1].forename)
+            // this.setState({ students })
+          })
+          .catch((error) => {
+              console.log(error)
+          })
+
+
+//EMPLOYEES FETCH
+          axios.get(url2, {
+            headers: {
+                'Authorization': `Bearer ${token}` 
+              }
+        })
+          .then(res => {
+            // let students = res.data;
+            this.setState({ 
+                employees: res.data.data, 
+                // employeesClasses: res.data.meta.includes
+            });
+            
+            console.log(this.state.employees)
+            // console.log(res.data)
+            console.log(res.data.data[1].classes.data[0].name)
+          })
+          .catch((error) => {
+              console.log(error)
+          })
+
+
+
+//CLASSES FETCH
+          axios.get(url3, {
+            headers: {
+                'Authorization': `Bearer ${token}` 
+              }
+        })
+          .then(res => {
+            // let students = res.data;
+            this.setState({ classesE: res.data.data });
+            console.log(this.state.classesE)
+            console.log(res.data.data[1].students.data[0].forename)
+          })
+          .catch((error) => {
+              console.log(error)
+          })
+
+      }
+
+
+  render() {
+      const { students } = this.state
+      const studentList = students.length ? (
+
+            students.map(student => {
+                return (
+                    <ul key={student.id}>
+                        <li>{student.forename} {student.surname}</li>
+                        <li>{student.id}</li>
+
+                    </ul>
+
+                )
+
+            })
+        ) : (
+            <div>Loading...</div>
+        ) 
+
+        const { employees, employeesClasses } = this.state
+        const employeeList = employees.length ? (
+            employees.map(employee => {
+                return (
+                    <ul key={employee.id}>
+                        <li>{employee.forename} {employee.surname}</li>
+                        {/* <li>{employee.id}</li> */}
+                        {/* <li>{ employees[1].classes.data[0].name }</li> */}
+                    </ul>
+
+                )
+            })
+
+
+            
+        ) : (
+            <div>Loading...</div>
+        )
+        const employeeClassesList = employeesClasses.map(employeeClass => {
+            return (
+                <div key={employeeClass}>{employeeClass}</div>
+            )
+        })
+
+
+
+
+        const { classesE } = this.state
+        const classList = classesE.length ? (
+            classesE.map(classE => {
+                return (
+                    <ul key={ classE.id }>
+                        <li>{ classE.name }</li>
+                        <li>{ classE.id }</li>
+                        {/* <li>{ classesE[1].students.data[0].forename } { classesE[1].students.data[0].surname }</li> */}
+                    </ul>
+                    
+                )
+            })
+        ) : (
+            <div>Loading...</div>
+        )
+      return (
+    <div className='data'>
+    <div>
+    <h2>Students</h2>
+       {studentList}
+
+    </div>
+    <div>
+    <h2>Employees</h2>
+       {employeeList}
+       {employeeClassesList}
+    </div> 
+    <div>
+    <h2>Classes</h2>
+       {classList}
+    </div>
+            {/* <ul>
+                {
+                    this.state.students.map(student =>
+                        <li key={ student.id }>
+                        { student.name }
+                        <br></br>
+                        { student.email }
+                        </li>
+                        )
+                }
+            </ul> */}
+      </div>
+    )
+  }
+}
